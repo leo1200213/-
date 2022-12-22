a52f09db1b46745522cc3287e5a8c1c8c2330f3c
blablabla891015 <83815454+blablabla891015@users.noreply.github.com>
2022-12-22 21:18:09 +0800

    visual

---
+++ visual/README.md
@@ -0,0 +1,17 @@
+## Clone the Project
+
+```git clone ${git_url}```
+
+To clone the entire project.
+
+## Install Packages
+turn on a terminal
+
+```npm install -g http-server```  
+```cd visual && yarn install``` 
+
+## Start
+under visual/,type
+```http-server``` on a terminal and open http://localhost:8080/
+
+
---
+++ visual/sketch.js
@@ -0,0 +1,52 @@
+var fade
+var fadeAmount = 1
+var mode="in"
+function setup() {
+  createCanvas(400, 400);
+  background(100,100,100,0.1);
+  fade=0
+}
+function cir(a,b,size,start_x,start_y,count,speed,mode){
+  if(count>0){
+    push()
+    let x
+    let y
+    if(mode==="in"){
+      x=(a-frameCount%a)*cos(frameCount*speed)+start_x
+      y=(b-frameCount%b)*sin(frameCount*speed)+start_y
+      
+    }
+    else{
+      x=(0+frameCount%a)*cos(frameCount*speed)+start_x
+      y=(0+frameCount%b)*sin(frameCount*speed)+start_y
+    }
+    // background(100,100,100);
+    strokeWeight(0.3)
+    let color= frameCount%255+10
+    fill(color,color,color)
+    ellipse(x,y,size)
+    
+    if (fade<0) fadeAmount=1; 
+ 
+    if (fade>255) fadeAmount=-10; 
+
+    fade += fadeAmount; 
+    pop()
+    if(x===a){
+      mode="in"
+    }
+    else{
+      mode="out"
+    }
+    cir(a/2,b/2,size/2,start_x,start_y,count-1,-speed)
+  }
+  else{
+    return
+  }
+}
+function draw() {
+  background(100,100,100,40);
+  cir(200,200,50,200,200,8,1,mode)
+  
+  
+}
