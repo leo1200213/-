38ec929f44a213394c464ef5d88bad62958461c8
blablabla891015 <83815454+blablabla891015@users.noreply.github.com>
2022-12-24 22:59:47 +0800

    visual

--- interface_withWebSocket/sketch.js
+++ interface_withWebSocket/sketch.js
@@ -1,9 +1,27 @@
-var fade
 var fadeAmount = 1
 var mode="in"
+var R
+var G
+var B
+var window_size=400
+var _brightness=200
+
+// Changeable////////
+var rotaion_width=window_size*6/7
+var rotaion_height=window_size*4/5
+var cir_size=50
+var layer=5 //1~5
+var rotation_speed=1 
+var afterimage=40 //殘影
+var _brightness_min=100 //0~255
+///////////////////
+
 function setup() {
-  createCanvas(400, 400);
-  background(100,100,100,0.1);
+  createCanvas(window_size, window_size);
+  R=frameCount%(_brightness/2)+_brightness_min
+  G=frameCount%(_brightness/3)+_brightness_min
+  B=frameCount%(_brightness/5)+_brightness_min
+  background(R,G,B,0.1);
   fade=0
 }
 function cir(a,b,size,start_x,start_y,count,speed,mode){
@@ -11,6 +29,10 @@ function cir(a,b,size,start_x,start_y,count,speed,mode){
     push()
     let x
     let y
+    R=frameCount%(_brightness/2)+_brightness_min
+    G=frameCount%(_brightness/3)+_brightness_min
+    B=frameCount%(_brightness/5)+_brightness_min
+    fill(R,G,B)
     if(mode==="in"){
       x=(a-frameCount%a)*cos(frameCount*speed)+start_x
       y=(b-frameCount%b)*sin(frameCount*speed)+start_y
@@ -22,9 +44,14 @@ function cir(a,b,size,start_x,start_y,count,speed,mode){
     }
     // background(100,100,100);
     strokeWeight(0.3)
-    let color= frameCount%255+10
-    fill(color,color,color)
-    ellipse(x,y,size)
+    
+    let cir_or_rect=Math.random()
+    if(cir_or_rect>0.5){
+      ellipse(x,y,size)
+    }
+    else{
+      rect(x-size/2,y-size/2,size)
+    }
     
     if (fade<0) fadeAmount=1; 
  
@@ -45,8 +72,11 @@ function cir(a,b,size,start_x,start_y,count,speed,mode){
   }
 }
 function draw() {
-  background(100,100,100,40);
-  cir(200,200,50,200,200,8,1,mode)
+  R=frameCount%(_brightness/2)+_brightness_min
+  G=frameCount%(_brightness/3)+_brightness_min
+  B=frameCount%(_brightness/5)+_brightness_min
+  background(R,G,B,afterimage);
+  cir(rotaion_width,rotaion_height,cir_size,window_size/2,window_size/2,layer,rotation_speed,mode)
   
   
 }
